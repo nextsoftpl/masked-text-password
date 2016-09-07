3ef70d6df14b4dafd93f2a45b5ee94988b09f3dc
nextsoftpl <nextsoft@nextsoft.pl>
2016-09-07 08:29:21 +0200

    angular directive

--- maskedTextPassword.directive.js
+++ maskedTextPassword.directive.js
@@ -1,5 +1,38 @@
+(function(angular){
+    'use strict';
+    angular.module('nextsoft-directives', []).directive('maskedTextPassword', maskedTextPassword);
+    function maskedTextPassword(){
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            scope: {
+                ngModel: '=',
+                targetName: '='
+            },
+            link: function (scope, element, attrs, ngModelCtrl) {
+                console.log(attrs);
+                var picker = element.maskedTextPassword();
+                //ngModelCtrl.$setViewValue(picker.getDate());
+                //model->view
+                // ngModelCtrl.$render(function () {
+                //     console.log('ngModelCtrl.$viewValue@' + ngModelCtrl.$viewValue);
+                //     picker.setDate(ngModelCtrl.$viewValue || '');
+                // });
+                // picker.on('dp.change', function (e) {
+                //     console.log('dp.change' + e.date);
+                //     scope.$apply(function () {
+                //         ngModelCtrl.$setViewValue(e.date);
+                //     });
+                // });
+            }
+        };
+    }
+
+})();
+
+
 (function ($) {
-    $.fn.maskedTextPassword = function (tN, debug) {
+    $.fn.maskedTextPassword = function (tN) {
 
         var h = [];
         var $this = $(this);
